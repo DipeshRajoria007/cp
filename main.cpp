849f24ad5ddb5456a8edabdb480e20f64023e973
Dipesh Rajoria <dipeshrajoria45@gmail.com>
2024-02-06 02:58:56 +0530

    cp setup

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endl
+#define incr_loop(a, n) for (i = a; i < n; i++)
+#define decr_loop(a, b) for (i = a; i > b; i--)
+#define nested_incr_loop(a, b) for (int j = a; j < b; j++)
+#define nested_decr_loop(a, b) for (int j = b; j > a; --j)
+typedef long long ll;
+#define pb push_back
+#define ppb pop_back
+#define MP make_pair
+#define sort_all(v) sort(all(v));
+#define PI 3.141592653589793238462
+#define space cout << ' ';
+#define gcd(a, b) __gcd(a, b);
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define mod 1000000007
+#define inf 1e18
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<long long, long long>>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define vvc vector<vector<char>>
+#define vvs vector<vector<string>>
+#define vs vector<string>
+#define vvc vector<vector<char>>
+#define vc vector<char>
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+
+
+void solve (){
+
+}
+
+int main() {
+    int t = 1 ;
+    // cin >> t;
+    while(t--) {
+        solve();
+        cout << endl;
+    }    
+}
+
